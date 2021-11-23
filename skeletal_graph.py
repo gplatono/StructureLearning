e8f1318bb91514ea33e0e2e151cbea424be70adc
David Wang <davidyuwang12@gmail.com>
2021-11-23 12:45:22 -0500

    added compute_edges function

--- skeletal_graph.py
+++ skeletal_graph.py
@@ -1,18 +1,41 @@
 import numpy as np
-
+from queue import LifoQueue
+import math
 
 class SkeletalGraph:
     def __init__(self, structure):
         self.vertices = structure
         self.compute_edges()
+        self.block_size = 1
 
     def compute_edges(self):
-        pass
+        # Add edge if blocks are within a certain radius of target block.
+        # Depth first searches through the set of vertices, adding close 
+        # vertices as edges.
+        edges = []
+        visited = {}
+        stack = LifoQueue()
+        stack.put(self.vertices[0])
+
+        while not stack.empty():
+            curr_vertex = stack.get()
+            for v in self.vertices:
+                # diistance between 2 verticies is < 1.3 * block_size and not visited
+                if self.block_size * 1.3 > math.sqrt((curr_vertex[0]-v[0])**2 \
+                + (curr_vertex[0]-v[0])**2 + (curr_vertex[0]-v[0])**2) \
+                and visited.get(curr_vertex, False) is False:
+                    edges.append([curr_vertex, v])
+                    stack.put(v)
+            visited[curr_vertex] = True
+        
+        return edges
 
     def add_edge(self, edge):
+        # add edges + vertex on an existing edge
         pass
 
     def remove_edges(self, edge):
+        # remove vertex + extra edges
         pass
 
     def get_intersecting_edges(self, x = None, y = None, z = None):
